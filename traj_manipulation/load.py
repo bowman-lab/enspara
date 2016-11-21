adcb8ba566620aa22ca80ba7558028a53406a349
Justin R. Porter <justinrporter@gmail.com>
2016-11-21 09:36:45 -0600

    Add ability to pass any kwargs valid for md.load into load_as_concatenated.

--- traj_manipulation/load.py
+++ traj_manipulation/load.py
@@ -10,11 +10,12 @@ import numpy as np
 import mdtraj as md
 
 
-def sound_trajectory(trj, top):
+def sound_trajectory(trj, **kwargs):
     '''
-    Determine the length of a trajectory on disk in log(n) time and
+    Determine the length of a trajectory on disk in log(n) (?) time and
     constant space by loading individual frames from disk at
-    exponentially increasing indices.
+    exponentially increasing indices. Additional keyword args are
+    passed on to md.load.
     '''
     search_space = [0, sys.maxsize]
     base = 2
@@ -25,7 +26,7 @@ def sound_trajectory(trj, top):
             frame = start+(base**iteration)
 
             try:
-                md.load(trj, top=top, frame=frame)
+                md.load(trj, frame=frame, **kwargs)
                 search_space[0] = frame
             except IndexError:
                 search_space[1] = frame
@@ -34,14 +35,16 @@ def sound_trajectory(trj, top):
     return search_space[1]
 
 
-def load_as_concatenated(filenames, topology, processes=None, debug=False):
+def load_as_concatenated(filenames, processes=None, debug=False, **kwargs):
     '''
     Load the given files from disk into a single numpy array. Returns a
-    tuple of trajectory lengths and xyz
+    tuple of trajectory lengths and xyz. All additional keyword args
+    are passed on to md.load (e.g. top, selection).
     '''
-    lengths = [sound_trajectory(f, topology) for f in filenames]
 
-    root_trj = md.load(filenames[0], top=topology, frame=0)
+    lengths = [sound_trajectory(f, **kwargs) for f in filenames]
+
+    root_trj = md.load(filenames[0], frame=0, **kwargs)
     shape = root_trj.xyz.shape
 
     # TODO: check all inputs against root
@@ -51,17 +54,19 @@ def load_as_concatenated(filenames, topology, processes=None, debug=False):
     shared_array = mp.Array(ctypes.c_double, reduce(mul, full_shape, 1),
                             lock=False)
 
-    proc_list = []
     with closing(mp.Pool(processes=processes, initializer=init,
                          initargs=(shared_array,))) as p:
-        proc_list.append(p.map_async(
-            partial(load_to_position, top=topology, arr_shape=full_shape),
+        proc = p.map_async(
+            partial(load_to_position, arr_shape=full_shape,
+                    load_kwargs=kwargs),
             zip([sum(lengths[0:i]) for i in range(len(lengths))],
                 filenames)
-            ))
+            )
 
     # gather exceptions.
-    [x.get() for x in proc_list]
+    proc.get()
+
+    # wait for termination
     p.join()
 
     xyz = tonumpyarray(shared_array).reshape(full_shape)
@@ -81,7 +86,7 @@ def tonumpyarray(mp_arr):
     return np.frombuffer(mp_arr)
 
 
-def load_to_position(spec, top, arr_shape):
+def load_to_position(spec, load_kwargs, arr_shape):
     '''
     Load a specified file into a specified position by spec, using the
     topology top. The arr_shape parameter lets us know how big the final
@@ -89,7 +94,7 @@ def load_to_position(spec, top, arr_shape):
     '''
     (position, filename) = spec
 
-    xyz = md.load(filename, top=top).xyz
+    xyz = md.load(filename, **load_kwargs).xyz
 
     # mp.Array must be converted to numpy array and reshaped
     arr = tonumpyarray(shared_array).reshape(arr_shape)
--- traj_manipulation/test.py
+++ traj_manipulation/test.py
@@ -20,7 +20,28 @@ class TestParallelLoad(unittest.TestCase):
         t2 = md.load(self.trj_fname, top=self.top)
         t3 = md.load(self.trj_fname, top=self.top)
 
-        lengths, xyz = load_as_concatenated([self.trj_fname]*3, self.top)
+        lengths, xyz = load_as_concatenated(
+            [self.trj_fname]*3,
+            top=self.top,
+            processes=7)
+        expected = np.concatenate([t1.xyz, t2.xyz, t3.xyz])
+
+        self.assertTrue(np.all(expected == xyz))
+        self.assertEqual(expected.shape, xyz.shape)
+
+    def test_selection_load_as_concatenated(self):
+
+        selection = np.array([1, 3, 6])
+
+        t1 = md.load(self.trj_fname, top=self.top, atom_indices=selection)
+        t2 = md.load(self.trj_fname, top=self.top, atom_indices=selection)
+        t3 = md.load(self.trj_fname, top=self.top, atom_indices=selection)
+
+        lengths, xyz = load_as_concatenated(
+            [self.trj_fname]*3,
+            top=self.top,
+            atom_indices=selection,
+            processes=3)
         expected = np.concatenate([t1.xyz, t2.xyz, t3.xyz])
 
         self.assertTrue(np.all(expected == xyz))
