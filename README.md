308652712e4608e5ed39669c1dc922a44a7c5382
Justin R. Porter <justinrporter@gmail.com>
2016-12-01 10:45:37 -0600

    Update readme for new testing, add cython compiled code to .gitignore, rm test.sh.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # stag
 Statistical Trajectory Analysis and Guidance
+
+# Developing
+
+## Testing
+
+Run
+
+```
+nosetests
+```
--- test.sh
+++
@@ -1,6 +0,0 @@
-python setup.py build_ext --inplace || { echo "Build failed."; exit 1;}
-find . -name '*.pyc' -delete
-
-for test in $( find . -name "test*.py" ); do
-  python -m $( echo $test | awk '{gsub("./stag", "stag"); gsub("/", "."); gsub(".py", ""); print}' ) || { echo "Test $test failed"; exit 1; };
-done
