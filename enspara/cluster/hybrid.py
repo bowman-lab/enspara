60a07d6a91dcdd103aec939ba679d7ce7e767ea2
Justin R. Porter <justinrporter@gmail.com>
2017-03-30 17:47:20 -0500

    Remove unused import.

--- enspara/cluster/hybrid.py
+++ enspara/cluster/hybrid.py
@@ -14,8 +14,7 @@ import numpy as np
 
 from .kcenters import kcenters
 from .kmedoids import _kmedoids_update
-from .util import _get_distance_method, ClusterResult, Clusterer, \
-    assign_to_nearest_center
+from .util import _get_distance_method, ClusterResult, Clusterer
 
 from ..exception import ImproperlyConfigured
 
