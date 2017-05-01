74e7111ff80b86e53f91f9857674c260a0e58770
Maxwell I. Zimmerman <mizimmer@wustl.edu>
2017-05-01 13:29:03 -0500

    added tests to calculating committors

--- enspara/tpt/test_fluxes.py
+++ enspara/tpt/test_fluxes.py
@@ -8,23 +8,39 @@ from numpy.testing import assert_array_equal
 from .committors import committors
 from .fluxes import fluxes
 
-Tij = np.array(
-    [
-        [0.5, 0.4, 0.1],
-        [0.25, 0.5, 0.25],
-        [0.1, 0.5, 0.4]])
 
 
 class Test_Fluxes(unittest.TestCase):
 
     def test_committors(self):
+        Tij1 = np.array(
+            [
+                [0.5, 0.4, 0.1],
+                [0.25, 0.5, 0.25],
+                [0.1, 0.5, 0.4]])
+
         true_committors = np.array([0, 0.5, 1.])
 
-        for_committors = committors(Tij, 0, 2)
+        for_committors = committors(Tij1, 0, 2)
         assert_array_equal(for_committors, true_committors)
 
-        for_committors = committors(Tij, [0], [2])
+        for_committors = committors(Tij1, [0], [2])
         assert_array_equal(for_committors, true_committors)
+
+        Tij2 = np.array(
+            [
+                [0.5, 0.4, 0.1, 0.],
+                [0.25, 0.5, 0.2, 0.05],
+                [0.1, 0.15, 0.5, 0.25],
+                [0., 0.1, 0.4, 0.5]])
+
+        true_committors = np.array([0, 0.34091, 0.60227, 1.])
+
+        for_committors = np.around(committors(Tij2, 0, 3), 5)
+        assert_array_equal(for_committors, true_committors)
+
+        for_committors = committors(Tij2, [0,2], [3])
+        assert_array_equal(for_committors, np.array([0, 0.1, 0, 1.0]))
 
     def test_fluxes(self):
         Tij = np.array(
