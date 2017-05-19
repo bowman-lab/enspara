89d8ee7a553a96fdff354ee31cf1060fde3b8778
Justin R. Porter <justinrporter@gmail.com>
2017-05-19 13:38:26 -0500

    structure classifiers as a list.

--- setup.py
+++ setup.py
@@ -6,18 +6,18 @@ import numpy as np
 
 __version__ = '0.0.0dev'
 
-CLASSIFIERS = """\
-Intended Audience :: Science/Research
-Intended Audience :: Developers
-Programming Language :: C
-Programming Language :: Python
-Programming Language :: Python :: 3
-Topic :: Scientific/Engineering :: Bio-Informatics
-Topic :: Scientific/Engineering :: Chemistry
-Operating System :: POSIX
-Operating System :: Unix
-Operating System :: MacOS
-"""
+CLASSIFIERS = [
+    "Development Status :: 2 - Pre-Alpha",
+    "Intended Audience :: Science/Research",
+    "Intended Audience :: Developers",
+    "Programming Language :: Cython",
+    "Programming Language :: Python :: 3 :: Only",
+    "Topic :: Scientific/Engineering :: Bio-Informatics",
+    "Topic :: Scientific/Engineering :: Chemistry",
+    "Operating System :: POSIX",
+    "Operating System :: Unix",
+    "Operating System :: MacOS",
+]
 
 # build cython with `python setup.py build_ext --inplace`
 
@@ -29,7 +29,7 @@ setup(
     name='enspara',
     version=__version__,
     platforms=['Linux', 'Mac OS-X', 'Unix'],
-    classifiers=CLASSIFIERS.splitlines(),
+    classifiers=CLASSIFIERS,
     include_dirs=[np.get_include()],
     ext_modules=cythonize(cython_extensions),
 )
