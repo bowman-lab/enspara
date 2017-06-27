2a77a9ad023f287a189d2dd6380709492c91a371
Maxwell I. Zimmerman <mizimmer@wustl.edu>
2017-06-27 10:41:59 -0500

    update comments on committors.py

--- enspara/tpt/committors.py
+++ enspara/tpt/committors.py
@@ -1,47 +1,29 @@
-# Author(s): TJ Lane (tjlane@stanford.edu) and Christian Schwantes
-#            (schwancr@stanford.edu)
-# Contributors: Vince Voelz, Kyle Beauchamp, Robert McGibbon
-# Copyright (c) 2014, Stanford University
-# All rights reserved.
+# Author(s): Maxwell Zimmerman 
 
 """
-Functions for computing forward committors for an MSM. The forward
-committor is defined for a set of sources and sink states, and for
-each state, the forward committor is the probability that a walker
-starting at that state will visit the sink state before the source
-state.
-
-These are some canonical references for TPT. Note that TPT
-is really a specialization of ideas very familiar to the
-mathematical study of Markov chains, and there are many
-books, manuscripts in the mathematical literature that
-cover the same concepts.
+Generation of committor probabilities. The forward committor
+probability, q+, for a state is the probability that it reaches a
+defined sink state(s) before it reaches a source state(s). The reverse
+committor, q-, is the probability of reaching the source state first; at
+equilibrium the forward and reverse committors are related by the
+following relation: q+ = 1 - q-
 
 References
 ----------
-.. [1] Weinan, E. and Vanden-Eijnden, E. Towards a theory of
-       transition paths. J. Stat. Phys. 123, 503-523 (2006).
-.. [2] Metzner, P., Schutte, C. & Vanden-Eijnden, E.
-       Transition path theory for Markov jump processes.
-       Multiscale Model. Simul. 7, 1192-1219 (2009).
-.. [3] Berezhkovskii, A., Hummer, G. & Szabo, A. Reactive
-       flux and folding pathways in network models of
-       coarse-grained protein dynamics. J. Chem. Phys.
-       130, 205102 (2009).
-.. [4] Noe, Frank, et al. "Constructing the equilibrium ensemble of folding
-       pathways from short off-equilibrium simulations." PNAS 106.45 (2009):
-       19011-19016.
+.. [1] Grinstead, C.M. and Snell, J.L., Introduction to Probability
+       American Mathematical Society Providence (2006)
 """
 from __future__ import print_function, division, absolute_import
 import numpy as np
 
-from mdtraj.utils.six.moves import xrange
-
-__all__ = ['committors', 'conditional_committors']
+__all__ = ['committors']
 
 def committors(tprob, sources, sinks):
     """
     Get the forward committors of the reaction sources -> sinks.
+    This is calculated by turning all sources and sinks into absorbing
+    states and calculating the probability or reaching one set of
+    aborbing states over the other, as covered in the above reference.
 
     Parameters
     ----------
@@ -54,25 +36,12 @@ def committors(tprob, sources, sinks):
 
     Returns
     -------
-    forward_committors : np.ndarray
+    committors : np.ndarray
         The forward committors for the reaction sources -> sinks
-
-    References
-    ----------
-    .. [1] Weinan, E. and Vanden-Eijnden, E. Towards a theory of
-           transition paths. J. Stat. Phys. 123, 503-523 (2006).
-    .. [2] Metzner, P., Schutte, C. & Vanden-Eijnden, E.
-           Transition path theory for Markov jump processes.
-           Multiscale Model. Simul. 7, 1192-1219 (2009).
-    .. [3] Berezhkovskii, A., Hummer, G. & Szabo, A. Reactive
-           flux and folding pathways in network models of
-           coarse-grained protein dynamics. J. Chem. Phys.
-           130, 205102 (2009).
-    .. [4] Noe, Frank, et al. "Constructing the equilibrium ensemble of folding
-           pathways from short off-equilibrium simulations." PNAS 106.45 (2009):
-           19011-19016.
     """
 
+    # set the data structure for sources, sinks, and every state that we will
+    # make absorbing
     sources = np.array(sources, dtype=int).reshape((-1, 1)).flatten()
     sinks = np.array(sinks, dtype=int).reshape((-1, 1)).flatten()
     all_absorbing = np.append(sources, sinks)
@@ -91,6 +60,6 @@ def committors(tprob, sources, sinks):
     I_m_Q[all_absorbing, all_absorbing] = 1.0
     
     # solves for committors: committors = N*R, where N = (I-Q)^-1
-    forward_committors = np.linalg.solve(I_m_Q, R).flatten()
+    committors = np.linalg.solve(I_m_Q, R).flatten()
 
-    return forward_committors
+    return committors
