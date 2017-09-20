2ebc077f34274978fe8ee2148a2d97b529e7614f
Justin R. Porter <justinrporter@gmail.com>
2017-09-20 10:50:57 -0500

    PEP8 and linting changes.

--- enspara/apps/feature_cluster.py
+++ enspara/apps/feature_cluster.py
@@ -3,22 +3,26 @@ import argparse
 import pickle
 import os
 
-from mdtraj import io
 from msmbuilder.libdistance import cdist
 
 from enspara import exception
 from enspara.cluster import KHybrid
 from enspara.util import array as ra
 
+
 class readable_dir(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
-        prospective_dir= os.path.dirname(os.path.abspath(values))
+        prospective_dir = os.path.dirname(os.path.abspath(values))
         if not os.path.isdir(prospective_dir):
-            raise argparse.ArgumentTypeError("readable_dir:{0} is not a valid path".format(prospective_dir))
+            raise argparse.ArgumentTypeError(
+                "readable_dir:{0} is not a valid path".format(prospective_dir))
         if os.access(prospective_dir, os.R_OK):
-            setattr(namespace,self.dest,values)
+            setattr(namespace, self.dest, values)
         else:
-            raise argparse.ArgumentTypeError("readable_dir:{0} is not a readable dir".format(prospective_dir))
+            raise argparse.ArgumentTypeError(
+                "readable_dir:{0} is not a readable dir".format(
+                    prospective_dir))
+
 
 def process_command_line(argv):
     parser = argparse.ArgumentParser(formatter_class=argparse.
@@ -70,9 +74,9 @@ def process_command_line(argv):
     elif args.cluster_distance.lower() == 'manhattan':
         args.cluster_distance = diff_manhattan
 
-
     a_file_exists = any(os.path.isfile(getattr(args, o)) for o in
-	['assignments', 'distances', 'center_indices', 'cluster_centers'])
+                        ['assignments', 'distances', 'center_indices',
+                         'cluster_centers'])
     if a_file_exists and not args.overwrite:
         raise FileExistsError
 
@@ -92,11 +96,9 @@ def diff_manhattan(trj, ref):
 def main(argv=None):
     args = process_command_line(argv)
 
-    keys = io.loadh(args.features).keys()
-
     try:
         features = ra.load(
-            args.features, keys=sorted(keys, key=lambda x: x.split('_')[-1]))
+            args.features, keys=...)
     except exception.DataInvalid:
         features = ra.load(args.features)
 
