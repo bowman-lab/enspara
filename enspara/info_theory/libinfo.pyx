2f2ee81b3d68331f007dba04ea185f6618a0b347
Justin R. Porter <justinrporter@gmail.com>
2018-03-17 13:22:25 -0500

    fix segmentation fault.

--- enspara/info_theory/libinfo.pyx
+++ enspara/info_theory/libinfo.pyx
@@ -30,16 +30,18 @@ def matrix_bincount2d_symmetrical(np.ndarray[DTYPE_t, ndim=2] a, int n):
     cdef np.ndarray[DTYPE_t, ndim=4] jc = np.zeros((a.shape[1], a.shape[1], n, n), dtype=DTYPE)
     cdef long a_row, b_row, i, j, t, k
     cdef unsigned int n_features = a.shape[1]
-    cdef unsigned int n_coords = int(((n_features)*(n_features-1))/2)
+    cdef unsigned int n_coords = int(((n_features)*(n_features+1))/2)
 
     # assemble an array of half-matrix coordinates
     cdef np.ndarray[DTYPE_t, ndim=2] coords = np.zeros((n_coords, 2), dtype=DTYPE)
+
     k = 0
-    for i in range(n_coords):
-        for j in range(i, n_coords):
+    for i in range(n_features):
+        for j in range(i, n_features):
             coords[k, 0] = i
             coords[k, 1] = j
-            k += 0
+            k += 1
+        
     k = 0
 
     assert a.shape[0] == a.shape[0]
