c4a3634b2ca4fd22d855593ed8eac95837aeb74a
Justin R. Porter <justinrporter@gmail.com>
2018-11-19 09:21:25 -0800

    Prinz MLE (#131)
    * Implement Prinz MLE based on msmb3.
    
    * Add Prinz MLE solver.
    
    * Add libmsm to setup.py
    
    * rm pip progress bar (but not quiet), add docs to setup.py
    
    * add upgrade pip step.
    
    * Force pip version in circleci.
    
    * include build_ext in circleci
    
    * Add inplace
    
    * Add tolerance to msm lib test.
    
    * Bump scikit-learn version

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
 import platform
-import os
-import subprocess
 import sys
 
 from distutils.core import setup
@@ -23,14 +21,14 @@ CLASSIFIERS = [
 
 # protect agaist absent cython/numpy
 try:
-    import numpy
+    import numpy as np
     import Cython
     if Cython.__version__ < '0.19':
         raise ImportError
     from Cython.Build import cythonize
 except ImportError:
-    print('-' * 80, file=sys.stderr)
-    print('\n'.join([
+    sys.stderr.write('-' * 80)
+    sys.stderr.write('\n'.join([
         'Error: building mdtraj requires numpy and cython>=0.19',
         'Try running the command ``pip install numpy cython`` or'
         '``conda install numpy cython``.',
@@ -38,7 +36,6 @@ except ImportError:
         'or see http://docs.scipy.org/doc/numpy/user/install.html and'
         'http://cython.org/ for more information.']))
 
-import numpy as np
 
 # this probably won't work for everyone. Works for me, though!
 # they'll need gcc 7 installed. Unfortunately, I don't have any idea how
@@ -68,6 +65,11 @@ cython_extensions = [
         ["enspara/geometry/libdist.pyx"],
         extra_compile_args=extra_compile_args,
         extra_link_args=extra_link_args,
+    ), Extension(
+        "enspara.msm.libmsm",
+        ["enspara/msm/libmsm.pyx"],
+        extra_compile_args=extra_compile_args,
+        extra_link_args=extra_link_args,
     )]
 
 setup(
@@ -82,18 +84,24 @@ setup(
     python_requires='>=3.5,<3.7',  # cython is broken for 3.7
     install_requires=[
         'Cython>=0.24',
-        'numpy>=1.14',
+        'numpy>=1.13',
         'tables>=3.2',
         'matplotlib>=1.5.1',
         'mdtraj>=1.7,<1.9',
-        'mpi4py',
-        'psutil',
-        'scikit-learn>=0.17.1',
+        'mpi4py>=2.0.0',
+        'psutil>=5.2.2',
+        'scikit-learn>=0.19.0',
         'scipy>=0.17'
     ],
     extras_require={
         'dev': [
             'nose',
+        ],
+        'docs': [
+            'Sphinx>=1.6.4',
+            'sphinx-rtd-theme>=0.2.4',
+            'sphinxcontrib-websupport>=1.0.1',
+            'numpydoc>=0.7.0',
         ]
     },
     zip_safe=False
