24605a314d98261aebd6bb39cc8ad3cfe483397b
Justin R. Porter <justinrporter@gmail.com>
2018-11-19 08:39:34 -0800

    Implement threaded hamming distance. (#135)

--- enspara/geometry/libdist.pyx
+++ enspara/geometry/libdist.pyx
@@ -14,9 +14,20 @@ ctypedef fused FLOAT_TYPE_T:
     np.float32_t
     np.float64_t
 
+ctypedef fused INTEGRAL_TYPE_T:
+    np.uint8_t
+    np.uint16_t
+    np.uint32_t
+    np.uint64_t
+    np.int8_t
+    np.int16_t
+    np.int32_t
+    np.int64_t
+
 cdef extern from "math.h" nogil:
     double sqrt(double x)
-    double abs(double x)
+    double fabs(double x)
+    float fabs(float x)
 
 def _check_is_2d(X):
     if len(X.shape) != 2:
@@ -63,6 +74,29 @@ def _prepare_for_2d_to_1d_distance(X, y, out):
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
+def _hamming(np.ndarray[INTEGRAL_TYPE_T, ndim=2] X,
+             np.ndarray[INTEGRAL_TYPE_T, ndim=1] y,
+             np.ndarray[np.float64_t, ndim=1] out):
+
+    cdef long n_samples = len(out)
+    cdef long n_features = len(y)
+    assert len(out) == X.shape[0], "Size of output array didn't match number of observations in X"
+    assert n_features == X.shape[1], "Number of features between X and y didn't match."
+
+    cdef long i, j = 0
+
+    for i in prange(n_samples, nogil=True):
+        out[i] = 0
+        for j in range(n_features):
+            if y[j] != X[i, j]:
+                out[i] += 1
+        out[i] /= n_features
+
+    return out
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
 def _manhattan(np.ndarray[FLOAT_TYPE_T, ndim=2] X,
                np.ndarray[FLOAT_TYPE_T, ndim=1] y,
                np.ndarray[np.float64_t, ndim=1] out):
@@ -78,7 +112,7 @@ def _manhattan(np.ndarray[FLOAT_TYPE_T, ndim=2] X,
 
     for i in prange(n_samples, nogil=True):
         for j in range(n_features):
-            out[i] += abs(X[i, j] - y[j])
+            out[i] += fabs(X[i, j] - y[j])
 
     return out.reshape(-1, 1)
 
@@ -146,4 +180,24 @@ def manhattan(X, y, out=None):
 
     out = _prepare_for_2d_to_1d_distance(X, y, out)
     _manhattan(X, y, out)
+    return out
+
+
+def hamming(X, y, out=None):
+    """Compute the Hamming distance between a point `y` and a group of
+    points `X`. Thread-parallized using OpenMP.
+
+    Parameters
+    ----------
+    X : array, shape=(n_samples, n_features)
+        The group of points for which to compute the distance from `y`.
+    y: array, shape=(n_features)
+        The point, for all rows in `X`, to compute the distance to.
+    out: array, shape=(n_samples), default=None
+        If provided, the array to place the distances in. If not provided,
+        an array will be allocated for you.
+    """
+
+    out = _prepare_for_2d_to_1d_distance(X, y, out)
+    _hamming(X, y, out)
     return out
