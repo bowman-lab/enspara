ac8f78db98f0a4eb214f7432b443e560e306db00
Justin R. Porter <justinrporter@gmail.com>
2018-12-14 15:47:53 -0600

    Remove mpi4py import protection (#142)
    * Remove import protection for mpi4py now that we require it.
    
    * Fix order of mpi/__init__.py

--- enspara/mpi/__init__.py
+++ enspara/mpi/__init__.py
@@ -1,16 +1,7 @@
-import warnings
+from mpi4py import *
 
-try:
-    from mpi4py import *
-
-    MPI_RANK = MPI.COMM_WORLD.Get_rank()
-    MPI_SIZE = MPI.COMM_WORLD.Get_size()
-except ImportError:
-    warnings.warn(ImportWarning, "Couldn't locate mpi4py!")
-
-    MPI_RANK = 0
-    MPI_SIZE = 1
-    raise
+MPI_RANK = MPI.COMM_WORLD.Get_rank()
+MPI_SIZE = MPI.COMM_WORLD.Get_size()
 
 from . import ops
 from . import io
