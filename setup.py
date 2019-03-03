80f4165c281d6dd24871d9b3690e5b3271582f36
Justin R. Porter <justinrporter@gmail.com>
2019-03-03 14:52:02 -0600

    Change pandas version.

--- setup.py
+++ setup.py
@@ -96,7 +96,7 @@ setup(
         'mdtraj>=1.7',
         'mpi4py>=2.0.0',
         'psutil>=5.2.2',
-        'pandas>=0.19.2',
+        'pandas>=0.20',
         'scikit-learn>=0.19.0',
         'scipy>=0.17'
     ],
