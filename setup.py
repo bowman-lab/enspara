1da88e912ab22777dcfa8f979fbaf681e16c9baf
longbowman <longbowman@protonmail.com>
2020-01-22 16:50:13 +0400

    split macos to pass all tests and upload source once and trying conda for new installation type

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 from distutils.core import setup
 from distutils.extension import Extension
 import distutils.ccompiler
-__version__ = '0.1.1rc21'
+__version__ = '0.1.1rc22'
 
 CLASSIFIERS = [
     "Development Status :: 3 - Alpha",
