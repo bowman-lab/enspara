eeb5b14c6cc37e78e4e6d588d0009e9756046463
longbowman <longbowman@protonmail.com>
2020-02-29 03:51:46 +0400

    adding directory for debug in the fork

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 from distutils.core import setup
 from distutils.extension import Extension
 import distutils.ccompiler
-__version__ = '0.1.1rc42'
+__version__ = '0.1.1rc43'
 
 CLASSIFIERS = [
     "Development Status :: 3 - Alpha",
