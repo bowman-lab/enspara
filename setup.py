61a3f62538d9be527cb58c818be0a8eb7045fba0
longbowman <longbowman@protonmail.com>
2020-04-16 09:37:24 +0400

    test jinja template

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 from distutils.core import setup
 from distutils.extension import Extension
 import distutils.ccompiler
-__version__ = '0.1.1rc65'
+__version__ = '0.1.1rc66'
 
 CLASSIFIERS = [
     "Development Status :: 3 - Alpha",
