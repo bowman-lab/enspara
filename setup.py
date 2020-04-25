da90c847050ca6f5339ed9a3cc7a5fb7ccdb1b80
longbowman <longbowman@protonmail.com>
2020-04-25 11:53:29 +0400

    remove numpy version from meta.yaml to pass import test

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 from distutils.core import setup
 from distutils.extension import Extension
 import distutils.ccompiler
-__version__ = '0.1.1rc89'
+__version__ = '0.1.1rc90'
 
 CLASSIFIERS = [
     "Development Status :: 3 - Alpha",
