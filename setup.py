ba6b56052970a2b03901a0483cfcc30fac3e8050
longbowman <longbowman@protonmail.com>
2020-05-12 20:51:51 +0400

    fix pytables dependency

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 from distutils.core import setup
 from distutils.extension import Extension
 import distutils.ccompiler
-__version__ = '0.1.1rc132'
+__version__ = '0.1.1rc133'
 
 CLASSIFIERS = [
     "Development Status :: 3 - Alpha",
