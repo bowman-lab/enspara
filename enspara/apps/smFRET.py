2eb032298044d4694aaa9c24a982958e776de112
Justin-J-Miller <jjmiller@wustl.edu>
2021-11-09 14:48:43 -0600

    added pycharm to gitignore, updated progress on app

--- enspara/apps/smFRET.py
+++ enspara/apps/smFRET.py
@@ -33,7 +33,7 @@ import mdtraj as md
 
 
 def process_command_line(argv):
-
+##Need to check whether these arguments are in fact parsed correctly, I took a first pass stab at this.
     parser = argparse.ArgumentParser(
         prog='FRET',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
@@ -51,14 +51,14 @@ def process_command_line(argv):
         help="transition probabilities from the MSM"
              "Should be of file type .npy")
         input_data_group.add_argument(
-        '--lagtime', nargs="+", action='append',
+        '--lagtime', nargs="+", action='append', type=float
         help="lag time used to construct the MSM (in ns)"
-             "Should be type float or int")        
+             "Should be type float")
         input_data_group.add_argument(
         '--resid_pairs', nargs="+", action='append',
         help="list of lists of residue pairs to sample, 1 indexed"
-             "e.g.: [[[1],[2]],[[2],[52]]]")            
- 
+             "e.g.: [[[1],[2]],[[2],[52]]]")
+
 
     # PARAMETERS
     FRET_args = parser.add_argument_group("FRET Settings")
@@ -83,7 +83,7 @@ def process_command_line(argv):
     cluster_args.add_argument(
         '--trj', nargs="+", required=False, action=readable_dir,
         help="Path to cluster centers from the MSM"
-             "should be of type .xtc. Not needed if supplying FRET dye distributions")       
+             "should be of type .xtc. Not needed if supplying FRET dye distributions")
     cluster_args.add_argument(
         '--FRET_dye_dist', nargs="+", required=False, action=readable_dir,
         help="Path to FRET dye distributions")
@@ -98,104 +98,106 @@ def process_command_line(argv):
         '--FRET_efficiencies', required=True, action=readable_dir,
         help="The location to write the predicted FRET efficiencies for each residue pair.")
 
-
-    args = parser.parse_args(argv[1:])
-
-    if args.features:
-        args.features = expand_files([args.features])[0]
-
-        if args.cluster_distance in FEATURE_DISTANCES:
-            args.cluster_distance = getattr(libdist, args.cluster_distance)
-        else:
-            raise exception.ImproperlyConfigured(
-                "The given distance (%s) is not compatible with features." %
-                args.cluster_distance)
-
-        if args.subsample != 1 and len(args.features) == 1:
-                raise exception.ImproperlyConfigured(
-                    "Subsampling is not supported for h5 inputs.")
-
-        # TODO: not necessary if mutually exclusvie above works
-        if args.trajectories:
-            raise exception.ImproperlyConfigured(
-                "--features and --trajectories are mutually exclusive. "
-                "Either trajectories or features, not both, are clustered.")
-        if args.topologies:
-            raise exception.ImproperlyConfigured(
-                "When --features is specified, --topology is unneccessary.")
-        if args.atoms:
-            raise exception.ImproperlyConfigured(
-                "Option --atoms is only meaningful when clustering "
-                "trajectories.")
-        if not args.cluster_distance:
-            raise exception.ImproperlyConfigured(
-                "Option --cluster-distance is required when clustering "
-                "features.")
-
-    elif args.trajectories and args.topologies:
-        args.trajectories = expand_files(args.trajectories)
-
-        if not args.cluster_distance or args.cluster_distance == 'rmsd':
-            args.cluster_distance = md.rmsd
-        else:
-            raise exception.ImproperlyConfigured(
-                "Option --cluster-distance must be rmsd when clustering "
-                "trajectories.")
-
-        if not args.atoms:
-            raise exception.ImproperlyConfigured(
-                "Option --atoms is required when clustering trajectories.")
-        elif len(args.atoms) == 1:
-            args.atoms = args.atoms * len(args.trajectories)
-        elif len(args.atoms) != len(args.trajectories):
-            raise exception.ImproperlyConfigured(
-                "Flag --atoms must be provided either once (selection is "
-                "applied to all trajectories) or the same number of times "
-                "--trajectories is supplied.")
-
-        if len(args.topologies) != len(args.trajectories):
-            raise exception.ImproperlyConfigured(
-                "The number of --topology and --trajectory flags must agree.")
-
-    else:
-        # CANNOT CLUSTER
-        raise exception.ImproperlyConfigured(
-            "Either --features or both of --trajectories and --topologies "
-            "are required.")
-
-    if args.cluster_radius is None and args.cluster_number is None:
-        raise exception.ImproperlyConfigured(
-            "At least one of --cluster-radius and --cluster-number is "
-            "required to cluster.")
-
-    if args.algorithm == 'kcenters':
-        args.Clusterer = KCenters
-        if args.cluster_iterations is not None:
-            raise exception.ImproperlyConfigured(
-                "--cluster-iterations only has an effect when using an "
-                "interative clustering scheme (e.g. khybrid).")
-    elif args.algorithm == 'khybrid':
-        args.Clusterer = KHybrid
-
-    if args.no_reassign and args.subsample == 1:
-        logger.warn("When subsampling is 1 (or unspecified), "
-                    "--no-reassign has no effect.")
-    if not args.no_reassign and mpi_mode and args.subsample > 1:
-        logger.warn("Reassignment is suppressed in MPI mode.")
-        args.no_reassign = True
-
-    if args.trajectories:
-        if os.path.splitext(args.center_features)[1] == '.h5':
-            logger.warn(
-                "You provided a centers file (%s) that looks like it's "
-                "an h5... centers are saved as pickle. Are you sure this "
-                "is what you want?")
-    else:
-        if os.path.splitext(args.center_features)[1] != '.npy':
-            logger.warn(
-                "You provided a centers file (%s) that looks like it's not "
-                "an npy, but this is how they are saved. Are you sure "
-                "this is what you want?" %
-                os.path.basename(args.center_features))
+    # Work greatly needed below! This is all just copy+paste from cluster.py's argparse
+    # args = parser.parse_args(argv[1:])
+    #
+    # if args.features:
+    #     args.features = expand_files([args.features])[0]
+    #
+    #     if args.cluster_distance in FEATURE_DISTANCES:
+    #         args.cluster_distance = getattr(libdist, args.cluster_distance)
+    #     else:
+    #         raise exception.ImproperlyConfigured(
+    #             "The given distance (%s) is not compatible with features." %
+    #             args.cluster_distance)
+    #
+    #     if args.subsample != 1 and len(args.features) == 1:
+    #             raise exception.ImproperlyConfigured(
+    #                 "Subsampling is not supported for h5 inputs.")
+    #
+    #     # TODO: not necessary if mutually exclusvie above works
+    #     if args.trajectories:
+    #         raise exception.ImproperlyConfigured(
+    #             "--features and --trajectories are mutually exclusive. "
+    #             "Either trajectories or features, not both, are clustered.")
+    #     if args.topologies:
+    #         raise exception.ImproperlyConfigured(
+    #             "When --features is specified, --topology is unneccessary.")
+    #     if args.atoms:
+    #         raise exception.ImproperlyConfigured(
+    #             "Option --atoms is only meaningful when clustering "
+    #             "trajectories.")
+    #     if not args.cluster_distance:
+    #         raise exception.ImproperlyConfigured(
+    #             "Option --cluster-distance is required when clustering "
+    #             "features.")
+    #
+    # elif args.trajectories and args.topologies:
+    #     args.trajectories = expand_files(args.trajectories)
+    #
+    #     if not args.cluster_distance or args.cluster_distance == 'rmsd':
+    #         args.cluster_distance = md.rmsd
+    #     else:
+    #         raise exception.ImproperlyConfigured(
+    #             "Option --cluster-distance must be rmsd when clustering "
+    #             "trajectories.")
+    #
+    #     if not args.atoms:
+    #         raise exception.ImproperlyConfigured(
+    #             "Option --atoms is required when clustering trajectories.")
+    #     elif len(args.atoms) == 1:
+    #         args.atoms = args.atoms * len(args.trajectories)
+    #     elif len(args.atoms) != len(args.trajectories):
+    #         raise exception.ImproperlyConfigured(
+    #             "Flag --atoms must be provided either once (selection is "
+    #             "applied to all trajectories) or the same number of times "
+    #             "--trajectories is supplied.")
+    #
+    #     if len(args.topologies) != len(args.trajectories):
+    #         raise exception.ImproperlyConfigured(
+    #             "The number of --topology and --trajectory flags must agree.")
+    #
+    # else:
+    #     # CANNOT CLUSTER
+    #     raise exception.ImproperlyConfigured(
+    #         "Either --features or both of --trajectories and --topologies "
+    #         "are required.")
+    #
+    # if args.cluster_radius is None and args.cluster_number is None:
+    #     raise exception.ImproperlyConfigured(
+    #         "At least one of --cluster-radius and --cluster-number is "
+    #         "required to cluster.")
+    #
+    # if args.algorithm == 'kcenters':
+    #     args.Clusterer = KCenters
+    #     if args.cluster_iterations is not None:
+    #         raise exception.ImproperlyConfigured(
+    #             "--cluster-iterations only has an effect when using an "
+    #             "interative clustering scheme (e.g. khybrid).")
+    # elif args.algorithm == 'khybrid':
+    #     args.Clusterer = KHybrid
+    #
+    # if args.no_reassign and args.subsample == 1:
+    #     logger.warn("When subsampling is 1 (or unspecified), "
+    #                 "--no-reassign has no effect.")
+    # if not args.no_reassign and mpi_mode and args.subsample > 1:
+    #     logger.warn("Reassignment is suppressed in MPI mode.")
+    #     args.no_reassign = True
+    #
+    # if args.trajectories:
+    #     if os.path.splitext(args.center_features)[1] == '.h5':
+    #         logger.warn(
+    #             "You provided a centers file (%s) that looks like it's "
+    #             "an h5... centers are saved as pickle. Are you sure this "
+    #             "is what you want?")
+    # else:
+    #     if os.path.splitext(args.center_features)[1] != '.npy':
+    #         logger.warn(
+    #             "You provided a centers file (%s) that looks like it's not "
+    #             "an npy, but this is how they are saved. Are you sure "
+    #             "this is what you want?" %
+    #             os.path.basename(args.center_features))
 
     return args
+
+
