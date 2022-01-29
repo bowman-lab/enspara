a8804e6063b4c7cbc6d58576d5168377b92457ec
Justin Miller <jjmiller@bowmore.seas.wustl.edu>
2022-01-28 18:09:08 -0600

    cleaned comments, need to test argparse still

--- enspara/apps/smFRET.py
+++ enspara/apps/smFRET.py
@@ -60,7 +60,13 @@ def process_command_line(argv):
         help="transition probabilities from the MSM"
              "Should be of file type .npy")
     input_args.add_argument(
-        '--lagtime', type=float, required=True,
+        '--photon_times', required=True,
+        help="List of lists of photon arrival times."
+             "each list is an individual photon burst"
+             "with cumulative arrival times (in us) for each burst"
+             "Should be of file type .npy")    
+    input_args.add_argument(
+        '--lagtime', type=int, required=True,
         help="lag time used to construct the MSM (in ns)"
              "Should be type float")
     input_args.add_argument(
@@ -68,9 +74,9 @@ def process_command_line(argv):
         help="residues to model FRET dyes on. Pass 2 residue pairs, same numbering as"
              "in the topology file. Pass multiple times to model multiple residue pairs"
              "e.g. --resid_pairs 1 5"
-             "--resid_pairs 5 86"
+             "--resid_pairs 5 86")
     input_args.add_argument(
-        '--FRET_dye_dists', nargs="+", required=False,
+        '--FRET_dye_dists', nargs="+", required=False, action=readable_dir,
         help="Path to FRET dye distributions")    
 
 
@@ -95,149 +101,44 @@ def process_command_line(argv):
     output_args = parser.add_argument_group("Output Settings")
 
     output_args.add_argument(
-        '--FRET_dye_distributions', required=False, action=readable_dir,
-        help="The location to write the FRET dye distributions.")
-    output_args.add_argument(
         '--FRET_output_dir', required=False, action=readable_dir, default='./',
         help="The location to write the FRET dye distributions.")
-    output_args.add_argument(
-        '--FRET_output_names', required=True,
-        help="Naming pattern for output FRET values.")
 
-    # Work greatly needed below! This is all just copy+paste from cluster.py's argparse
-    # Ideally should have some checks (e.g. can't supply only centers.xtc and not top)
     args = parser.parse_args(argv[1:])
-
-    #Minimum checks- 
-    #see if need to do FRET_dye modeling, FE, or both
-    #Check to make sure all arguments are provided
-    #Return helpful errors?
-
-
-    #
-    #     if args.cluster_distance in FEATURE_DISTANCES:
-    #         args.cluster_distance = getattr(libdist, args.cluster_distance)
-    #     else:
-    #         raise exception.ImproperlyConfigured(
-    #             "The given distance (%s) is not compatible with features." %
-    #             args.cluster_distance)
-    #
-    #     if args.subsample != 1 and len(args.features) == 1:
-    #             raise exception.ImproperlyConfigured(
-    #                 "Subsampling is not supported for h5 inputs.")
-    #
-    #     #TODO: not necessary if mutually exclusive above works
-    #     if args.trajectories:
-    #         raise exception.ImproperlyConfigured(
-    #             "--features and --trajectories are mutually exclusive. "
-    #             "Either trajectories or features, not both, are clustered.")
-    #     if args.topologies:
-    #         raise exception.ImproperlyConfigured(
-    #             "When --features is specified, --topology is unneccessary.")
-    #     if args.atoms:
-    #         raise exception.ImproperlyConfigured(
-    #             "Option --atoms is only meaningful when clustering "
-    #             "trajectories.")
-    #     if not args.cluster_distance:
-    #         raise exception.ImproperlyConfigured(
-    #             "Option --cluster-distance is required when clustering "
-    #             "features.")
-    #
-    # elif args.trajectories and args.topologies:
-    #     args.trajectories = expand_files(args.trajectories)
-    #
-    #     if not args.cluster_distance or args.cluster_distance == 'rmsd':
-    #         args.cluster_distance = md.rmsd
-    #     else:
-    #         raise exception.ImproperlyConfigured(
-    #             "Option --cluster-distance must be rmsd when clustering "
-    #             "trajectories.")
-    #
-    #     if not args.atoms:
-    #         raise exception.ImproperlyConfigured(
-    #             "Option --atoms is required when clustering trajectories.")
-    #     elif len(args.atoms) == 1:
-    #         args.atoms = args.atoms * len(args.trajectories)
-    #     elif len(args.atoms) != len(args.trajectories):
-    #         raise exception.ImproperlyConfigured(
-    #             "Flag --atoms must be provided either once (selection is "
-    #             "applied to all trajectories) or the same number of times "
-    #             "--trajectories is supplied.")
-    #
-    #     if len(args.topologies) != len(args.trajectories):
-    #         raise exception.ImproperlyConfigured(
-    #             "The number of --topology and --trajectory flags must agree.")
-    #
-    # else:
-    #     # CANNOT CLUSTER
-    #     raise exception.ImproperlyConfigured(
-    #         "Either --features or both of --trajectories and --topologies "
-    #         "are required.")
-    #
-    # if args.cluster_radius is None and args.cluster_number is None:
-    #     raise exception.ImproperlyConfigured(
-    #         "At least one of --cluster-radius and --cluster-number is "
-    #         "required to cluster.")
-    #
-    # if args.algorithm == 'kcenters':
-    #     args.Clusterer = KCenters
-    #     if args.cluster_iterations is not None:
-    #         raise exception.ImproperlyConfigured(
-    #             "--cluster-iterations only has an effect when using an "
-    #             "interative clustering scheme (e.g. khybrid).")
-    # elif args.algorithm == 'khybrid':
-    #     args.Clusterer = KHybrid
-    #
-    # if args.no_reassign and args.subsample == 1:
-    #     logger.warn("When subsampling is 1 (or unspecified), "
-    #                 "--no-reassign has no effect.")
-    # if not args.no_reassign and mpi_mode and args.subsample > 1:
-    #     logger.warn("Reassignment is suppressed in MPI mode.")
-    #     args.no_reassign = True
-    #
-    # if args.trajectories:
-    #     if os.path.splitext(args.center_features)[1] == '.h5':
-    #         logger.warn(
-    #             "You provided a centers file (%s) that looks like it's "
-    #             "an h5... centers are saved as pickle. Are you sure this "
-    #             "is what you want?")
-    # else:
-    #     if os.path.splitext(args.center_features)[1] != '.npy':
-    #         logger.warn(
-    #             "You provided a centers file (%s) that looks like it's not "
-    #             "an npy, but this is how they are saved. Are you sure "
-    #             "this is what you want?" %
-    #             os.path.basename(args.center_features))
-
+    #Add error checkers?? None come to mind at the moment...
     return args
 
 
 def main(argv=None):
 
     args = process_command_line(argv)
-    print(args)
-#     #Calculate the FRET efficiencies
-#     t_probabilties= np.load('####ARGPARSE FOR T_probs')
-#     logger.info("Loaded t_probs from %s" ##ARGPARSE for t_probs)
-#     populations=np.load('#####ARGPARSE FOR EQ PROBS')
-#     logger.info("Loaded eq_probs from %s" ##ARGPARSE for eq_probs)
+    #Calculate the FRET efficiencies
+    t_probabilties= np.load(args.t_probs)
+    logger.info(f"Loaded t_probs from {args.t_probs}")
+    populations=np.load(args.eq_probs)
+    logger.info(f"Loaded eq_probs from {args.eq_probs}")
+    resSeq_pairs=np.array(args.resid_pairs)
+    cumulative_times=np.load(args.photon_times)
+    
+    conversion_factor=1000/(args.lagtime*args.slowing_factor)#Multiply experimental wait times by this to get MSM steps.
+    MSM_frames=np.array([np.multiply(cumulative_times[i], conversion_factor).astype(int) for i in range(len(cumulative_times))])
 
 
-    # for n in np.arange(resSeq_pairs.shape[0]):
-    #     title = '%sC_%sC' % (resSeq_pairs[n,0], resSeq_pairs[n,1])
-    #     probs_file = "%s/probs_%s.h5" % (FRET_ensemble_folder, title) 
-    #     bin_edges_file = "%s/bin_edges_%s.h5" % (FRET_ensemble_folder, title)
-    #     probs = ra.load(probs_file)
-    #     bin_edges = ra.load(bin_edges_file)
-    #     dist_distribution = make_distribution(probs, bin_edges)
-    #     FEs_sampling = dyes_from_expt_dist.sample_FRET_histograms(
-    #         T=T, populations=populations, dist_distribution=dist_distribution,
-    #         MSM_frames=MSM_frames, n_photon_std=n_photon_std, n_procs=n_procs)
-    #     np.save("%s/FE_mcmc_histogram_%s_time_tuner_%d.npy" % (output_folder, title, Slowing_factor), FEs_sampling)
+    for n in np.arange(resSeq_pairs.shape[0]):
+        title = f'{resSeq_pairs[n,0]}_{resSeq_pairs[n,1]}'
+        probs_file = f"{args.FRET_dye_dists}/probs_{title}.h5"
+        bin_edges_file = f"{args.FRET_dye_dists}/bin_edges_{title}.h5"
+        probs = ra.load(probs_file)
+        bin_edges = ra.load(bin_edges_file)
+        dist_distribution = make_distribution(probs, bin_edges)
+        FEs_sampling = dyes_from_expt_dist.sample_FRET_histograms(
+            T=t_probabilties, populations=populations, dist_distribution=dist_distribution,
+            MSM_frames=MSM_frames, n_photon_std=args.n_chunks, n_procs=args.n_procs)
+        np.save(f"{FRET_output_dir}/{FRET_output_names}_{title}_time_factor_{args.slowing_factor}.npy", FEs_sampling)
 
 
-    # logger.info("Success! Calculated FRET distributions your input parameters can be found here: %s" % (output_folder + 'FRET_from_exp.json'))
-    # print(json.dumps(args.__dict__,  output_folder+'FRET_from_expt.json',indent=4))
+    logger.info(f"Success! Calculated FRET distributions your input parameters can be found here: {FRET_output_dir}_FRET_inputs.json"
+    print(json.dumps(args.__dict__,  output_folder+'FRET_inputs.json',indent=4))
 
 
 if __name__ == "__main__":
