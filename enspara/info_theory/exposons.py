72019c622d165b363c2b4ea1cb8df28eb3775a78
Justin R. Porter <justinrporter@gmail.com>
2022-02-07 08:32:07 -0600

    modify exposons code to handle identifying sidechain atoms differently.

--- enspara/info_theory/exposons.py
+++ enspara/info_theory/exposons.py
@@ -124,6 +124,48 @@ def exposons_from_sasas(sasas, damping, weights, threshold):
     return sasa_mi, c.labels_
 
 
+def get_sidechain_atom_ids(top):
+    """Discover the atom IDs for atoms that are in sidechains.
+
+    Looks for atoms that are NOT named N, C, CA, O, HA, H, H1,
+    H2, H3 or OXT.
+
+    Parameters
+    ----------
+    top: md.Topology
+        Topology object that supplies names for each atom.
+
+    Returns
+    -------
+    sc_ids: list
+        List of np.ndarray objects, each containing the atom ids belonging
+        to each residue.
+    """
+
+    SELECTION = ('not (name N or name C or name CA or name O or '
+                 'name HA or name H or name H1 or name H2 or name '
+                 'H3 or name OXT)')
+
+    sc_ids = []
+    for i in range(top.n_residues):
+        sstr = f'resid {i} and {SELECTION}'
+        try:
+            ids = top.select(sstr)
+        except RecursionError:
+            print("Failed with RecursionError on residue index", i, "with querystring:")
+            print('"'+sstr+"'")
+            import pickle
+
+            with open('toppickle.top', 'wb') as f:
+                pickle.dump(top, f)
+
+            raise
+
+        sc_ids.append(ids)
+
+    return sc_ids
+
+
 @cite('exposons')
 def condense_sidechain_sasas(sasas, top):
     """Condense atomic SASAs into sidechain SASAs.
@@ -145,12 +187,7 @@ def condense_sidechain_sasas(sasas, top):
     assert top.n_residues > 1
     assert top.n_atoms == sasas.shape[1]
 
-    SELECTION = ('not (name N or name C or name CA or name O or '
-                 'name HA or name H or name H1 or name H2 or name '
-                 'H3 or name OXT)')
-
-    sc_ids = [top.select(f'resid {i} and ( {SELECTION} )')
-              for i in range(top.n_residues)]
+    sc_ids = get_sidechain_atom_ids(top)
 
     rsd_sasas = np.zeros((sasas.shape[0], len(sc_ids)), dtype='float32')
 
--- enspara/test/test_exposons.py
+++ enspara/test/test_exposons.py
@@ -3,31 +3,50 @@ import mdtraj as md
 
 from numpy.testing import assert_array_equal, assert_allclose
 
-from enspara.info_theory.exposons import exposons
+from enspara.info_theory import exposons
 from .util import get_fn
 
 
-class TestExposonsLoad(unittest.TestCase):
+def test_exposons_pipeline_weighting():
 
-    def setUp(self):
-        self.trj = md.load(get_fn('beta-peptide.xtc'),
-                           top=get_fn('beta-peptide.pdb'))
+    trj = md.load(get_fn('beta-peptide.xtc'),
+                      top=get_fn('beta-peptide.pdb'))
 
-    def test_exposons_pipeline_weighting(self):
+    repeat_trj = md.join([trj[0:3], trj[0:3], trj[3:6]])
+    norepeat_trj = md.join([trj[0:3], trj[3:6]])
 
-        repeat_trj = md.join([self.trj[0:3], self.trj[0:3], self.trj[3:6]])
-        norepeat_trj = md.join([self.trj[0:3], self.trj[3:6]])
+    unweighted_mi, unweighted_exp = exposons.exposons(
+            repeat_trj, damping=0.9, threshold=1.0)
+    weighted_mi, weighted_exp = exposons.exposons(
+            norepeat_trj, damping=0.9, threshold=1.0, weights=[2, 2, 2, 1, 1, 1])
 
-        unweighted_mi, unweighted_exp = exposons(repeat_trj, 0.9,
-                                                 threshold=1.0)
-        weighted_mi, weighted_exp = exposons(norepeat_trj, 0.9,
-                                             threshold=1.0,
-                                             weights=[2, 2, 2, 1, 1, 1])
-
-        assert_allclose(unweighted_mi, weighted_mi, rtol=1e-14)
-        assert_array_equal(unweighted_exp, weighted_exp)
-
-    # def test_exposons_noatom_warning(self):
+    assert_allclose(unweighted_mi, weighted_mi, rtol=1e-14)
+    assert_array_equal(weighted_exp, unweighted_exp)
 
 
-    #     unweighted_mi, unweighted_exp = exposons(repeat_trj, 0.9)
+def test_exposons_sidechain_selection():
+
+    trj = md.load(get_fn('beta-peptide.xtc'),
+                  top=get_fn('beta-peptide.pdb'))
+
+    repeat_trj = md.join([trj[0:3], trj[0:3], trj[3:6]])
+    norepeat_trj = md.join([trj[0:3], trj[3:6]])
+
+    expected_ids = [
+        [ 6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
+        [30, 31, 32, 33, 34, 35],
+        [42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52],
+        [59, 60, 61, 62],
+        [69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79],
+        [85, 86],
+        [ 93,  94,  95,  96,  97,  98,  99, 100, 101, 102, 103, 104, 105, 106, 107, 108],
+        [115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127],
+        [134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147],
+        [154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171]
+    ]
+
+    ids = exposons.get_sidechain_atom_ids(repeat_trj.top)
+
+    for obs, exp in zip(ids, expected_ids):
+        assert_array_equal(obs, exp)
+
