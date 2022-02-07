078f4e7d25f19f3efa6a4a2b530ab6cd48a07c4f
Justin R. Porter <justinrporter@gmail.com>
2022-02-07 17:31:35 -0600

    fix liberalization to python 3.9 to allow 3.9.1 and higher.

--- setup.py
+++ setup.py
@@ -104,7 +104,7 @@ setup(
     classifiers=CLASSIFIERS,
     include_dirs=[np.get_include()],
     ext_modules=cythonize(cython_extensions),
-    python_requires='>=3.5,<=3.9', 
+    python_requires='>=3.5,<3.10', 
     entry_points={'console_scripts': ['enspara = enspara.apps.main:main']},
     setup_requires=['Cython>=0.24', 'numpy>=1.13'],
     install_requires=install_requires,
