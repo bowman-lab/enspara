251786dab83059890a0c937d77b9d0de4232f087
Gavin John <gavinnjohn@gmail.com>
2025-07-08 12:18:23 -0400

    Add rewrite functionality to gen_meta.py

--- gen_meta.py
+++ gen_meta.py
@@ -2,8 +2,24 @@
 import jinja2
 import tomllib
 
+rewrites = {
+    "tables": "pytables",
+}
+
 with open("pyproject.toml", "rb") as f:
     pyproject = tomllib.load(f)
+
+print(pyproject)
+
+for original in rewrites:
+    rewritten = rewrites[original]
+    pyproject["project"]["dependencies"] = [ dep.replace(original, rewritten) for dep in pyproject["project"]["dependencies"] ]
+    pyproject["build-system"]["requires"] = [ dep.replace(original, rewritten) for dep in pyproject["build-system"]["requires"] ]
+    pyproject["project"]["optional-dependencies"] = {
+        deptype: [ dep.replace(original, rewritten) for dep in pyproject["project"]["optional-dependencies"][deptype] ]
+        for deptype in pyproject["project"]["optional-dependencies"]
+    }
+    pyproject["build-system"]["requires"].append("pip")
 
 env = jinja2.Environment(
     loader=jinja2.FileSystemLoader("."),
